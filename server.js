332c32bc5f504833bae55b6e6259b61d36b3b932
Ven Kim <venvex@gmail.com>
2021-09-04 16:39:01 -0400

    created server.js with const and functions

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const port = 8080;
+const mainDir = path.join(__dirname, "/public");
+
+app.use(express.static('publi'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(mainDir, "notes.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.get("/api/notes/:id", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    res.json(savedNotes[Number(req.params.id)]);
+});
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(mainDir, "index.html"));
+});
+
+app.post("/api/notes", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let newNote = req.body;
+    let uniqueID = (savedNotes.length).toString();
+    newNote.id = uniqueID;
+    savedNotes.push(newNote);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    console.log("note saved to db.json. Content: ", newNote);
+    res.json(savedNotes);
+})
